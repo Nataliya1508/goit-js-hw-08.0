2a958ab86aa2ae17657acf4350d30d3d9d63950d
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-07-31 08:27:57 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
 
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form')
+console.log(formEl);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+formEl.addEventListener('input', throttle(onFormInput,500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    formEl.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    
+}
+
+updateForm();
+
+function updateForm() {
+    if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+return
+    }
+    const savedForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+      formEl.elements.email.value = savedForm.email;
+      formEl.elements.message.value = savedForm.message;
+}
